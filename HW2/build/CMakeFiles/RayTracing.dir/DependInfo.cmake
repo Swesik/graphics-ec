f218a064453c71306b1d622da42e3339e9d02ed2
swesik <swesik@umich.edu>
2024-09-26 17:01:11 -0400

    added monkey-test + added build

---
+++ HW2/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/swesikramineni/github/eecs498-graphics/eecs498-graphics-p1/HW2/Accel.cpp" "CMakeFiles/RayTracing.dir/Accel.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/Accel.cpp.o.d"
+  "/Users/swesikramineni/github/eecs498-graphics/eecs498-graphics-p1/HW2/Math.cpp" "CMakeFiles/RayTracing.dir/Math.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/Math.cpp.o.d"
+  "/Users/swesikramineni/github/eecs498-graphics/eecs498-graphics-p1/HW2/Ray.cpp" "CMakeFiles/RayTracing.dir/Ray.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/Ray.cpp.o.d"
+  "/Users/swesikramineni/github/eecs498-graphics/eecs498-graphics-p1/HW2/Scene.cpp" "CMakeFiles/RayTracing.dir/Scene.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/Scene.cpp.o.d"
+  "/Users/swesikramineni/github/eecs498-graphics/eecs498-graphics-p1/HW2/main.cpp" "CMakeFiles/RayTracing.dir/main.cpp.o" "gcc" "CMakeFiles/RayTracing.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
